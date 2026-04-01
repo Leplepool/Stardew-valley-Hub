f3a654affdea067391fec40cd437a2c2c95405d5
Leplepool <felipeff27360953@gmail.com>
2026-04-01 16:21:58 -0300

    Create config.js file with configuration and data

---
+++ config.js
@@ -0,0 +1,18 @@
+// Configuration and data for the Stardew Valley Hub
+
+const config = {
+    currentDateTime: '2026-04-01 19:20:50',
+    userLogin: 'Leplepool',
+    gameSettings: {
+        maxPlayers: 4,
+        theme: 'default',
+        language: 'English'
+    },
+    serverSettings: {
+        host: 'localhost',
+        port: 8080,
+        enableLogging: true
+    }
+};
+
+module.exports = config;
